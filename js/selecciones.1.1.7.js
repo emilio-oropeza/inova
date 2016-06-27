f9ee4568b402fdf4ed8e05464389311ce63dca13
Vocho <vocho@underdog.gs>
2016-06-27 09:50:21 -0500

    sissxsx
    cdc

---
+++ js/selecciones.1.1.7.js
@@ -0,0 +1,421 @@
+var selecciones = [
+	{
+		"nombre": "Albania",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 5,
+				"goles": 1,
+				"jugadores": ["Armando Sadiku"]
+			}
+		]
+	},
+	{
+		"nombre": "Alemania",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 1,
+				"goles": 1,
+				"jugadores": ["Shkodran Mustafi"]
+			},
+			{
+				"marca": "adidas",
+				"modelo": 5,
+				"goles": 1,
+				"jugadores": ["Schweinsteiger"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 9,
+				"goles": 1,
+				"jugadores": ["Mario Gómez"]
+			}
+		]
+	},
+	{
+		"nombre": "Austria",
+		"tenis":[
+			{
+				"marca": "adidas",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Alessandro Schöpf"]
+			}
+		]
+	},
+	{
+		"nombre": "Bélgica",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 7,
+				"goles": 2,
+				"jugadores": ["Romelu Lukaku"]
+			},
+			{
+				"marca": "adidas",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Axel Witsel"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 4,
+				"goles": 1,
+				"jugadores": ["Radja Nainggolan"]
+			}
+		]
+	},
+	{
+		"nombre": "Croacia",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 4,
+				"goles": 4,
+				"jugadores": ["Luka Modrić", "Iván Perisic", "Kalinic"]
+			},
+			{
+				"marca": "adidas",
+				"modelo": 3,
+				"goles": 1,
+				"jugadores": ["Iván Rakitic"]
+			}
+		]
+	},
+	{
+		"nombre": "Eslovaquia",
+		"tenis":[
+			{
+				"marca": "adidas",
+				"modelo": 5,
+				"goles": 1,
+				"jugadores": ["Ondrej Duda"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 5,
+				"goles": 2,
+				"jugadores": ["Vladimír Weiss", "Marek Hamsík"]
+			}
+		]
+	},
+	{
+		"nombre": "España",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Gerard Piqué"]
+			},
+			{
+				"marca": "adidas",
+				"modelo": 0,
+				"goles": 3,
+				"jugadores": ["Alvaro Morata"]
+			},
+			{
+				"marca": "puma",
+				"modelo": 2,
+				"goles": 1,
+				"jugadores": ["Nolito"]
+			}
+		]
+	},
+	{
+		"nombre": "Francia",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 5,
+				"goles": 2,
+				"jugadores": ["Dimitri Payet"]
+			},			
+			{
+				"marca": "puma",
+				"modelo": 2,
+				"goles": 1,
+				"jugadores": ["Oliver Giroud"]
+			}
+			,			
+			{
+				"marca": "puma",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Antonie Griezmann"]
+			}
+		]
+	},
+	{
+		"nombre": "Gales",
+		"tenis":[
+			{
+				"marca": "adidas",
+				"modelo": 0,
+				"goles": 3,
+				"jugadores": ["Gareth Bale"]
+			},
+			{
+				"marca": "adidas",
+				"modelo": 5,
+				"goles": 1,
+				"jugadores": ["Hal Robson-Kanu"]
+			},
+			{
+				"marca": "newb",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Aaron Ramsey"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 2,
+				"goles": 1,
+				"jugadores": ["Neil Taylor"]
+			}
+		]
+	},
+	{
+		"nombre": "Hungría",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 7,
+				"goles": 1,
+				"jugadores": ["Adam Szalai"]
+			},
+			{
+				"marca": "puma",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Zlotan Stieber"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 5,
+				"goles": 1,
+				"jugadores": ["Balázs Dzsudzsák"]
+			},
+			{
+				"marca": "mizuno",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Zoltán Gera"]
+			}
+		]
+	},
+	{
+		"nombre": "Inglaterra",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 2,
+				"goles": 1,
+				"jugadores": ["Eric Dier"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 9,
+				"goles": 2,
+				"jugadores": ["Jaimie Vardy", "Daniel Sturridge"]
+			}
+		]
+	},
+	{
+		"nombre": "Irlanda del Norte",
+		"tenis":[
+			{
+				"marca": "adidas",
+				"modelo": 1,
+				"goles": 2,
+				"jugadores": ["Gareth Mcauley", "Niall Mcginn"]
+			}
+		]
+	},
+	{
+		"nombre": "Islandia",
+		"tenis":[
+			{
+				"marca": "adidas",
+				"modelo": 1,
+				"goles": 1,
+				"jugadores": ["Bjarnason"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 1,
+				"goles": 1,
+				"jugadores": ["Gylfi Sigurasson"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 4,
+				"goles": 1,
+				"jugadores": ["Jón Dadi Bödvarsson"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 5,
+				"goles": 1,
+				"jugadores": ["Arnór Traustason"]
+			}
+		]
+	},
+	{
+		"nombre": "Italia",
+		"tenis":[
+			{
+				"marca": "adidas",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Emanuele Giaccherini"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 5,
+				"goles": 2,
+				"jugadores": ["Graziano Pellè", "Edér"]
+			}
+		]
+	},
+	{
+		"nombre": "Polonia",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 7,
+				"goles": 1,
+				"jugadores": ["Arkadiusz Milik"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 5,
+				"goles": 1,
+				"jugadores": ["Blaszczykowski"]
+			}
+		]
+	},
+	{
+		"nombre": "Portugal",
+		"tenis":[
+			{
+				"marca": "adidas",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Nani"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 5,
+				"goles": 2,
+				"jugadores": ["Cristiano Ronaldo"]
+			}
+		]
+	},
+	{
+		"nombre": "República Checa",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 4,
+				"goles": 1,
+				"jugadores": ["Milen Skoda"]
+			},
+			{
+				"marca": "adidas",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Milen Skoda"]
+			}
+		]
+	},
+	{
+		"nombre": "República de Irlanda",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Wes Hoolahan"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 4,
+				"goles": 1,
+				"jugadores": ["Robbie Brady"]
+			}
+		]
+	},
+	{
+		"nombre": "Rumanía",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 3,
+				"goles": 2,
+				"jugadores": ["Bogdan Stancu"]
+			}
+		]
+	},
+	{
+		"nombre": "Rusia",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 4,
+				"goles": 1,
+				"jugadores": ["Vasili Berezutskiy"]
+			},
+			{
+				"marca": "adidas",
+				"modelo": 1,
+				"goles": 1,
+				"jugadores": ["Denis Glushakov"]
+			}
+		]
+	},
+	{
+		"nombre": "Suecia",
+		"tenis":[]
+	},
+	{
+		"nombre": "Suiza",
+		"tenis":[
+			{
+				"marca": "adidas",
+				"modelo": 1,
+				"goles": 1,
+				"jugadores": ["Fabian Schär"]
+			},
+			{
+				"marca": "nike",
+				"modelo": 1,
+				"goles": 1,
+				"jugadores": ["Admír Nehmedi"]
+			}
+		]
+	},
+	{
+		"nombre": "Turquía",
+		"tenis":[
+			{
+				"marca": "nike",
+				"modelo": 4,
+				"goles": 1,
+				"jugadores": ["Burak Yilmaz"]
+			},
+			{
+				"marca": "adidas",
+				"modelo": 0,
+				"goles": 1,
+				"jugadores": ["Ozan Tufan"]
+			}
+		]
+	},
+	{
+		"nombre": "Ucrania",
+		"tenis":[]
+	}
+];
